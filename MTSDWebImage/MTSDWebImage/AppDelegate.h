33a440da56d362975272a1d8783a6c97e4595a16
Lollypop-zmt <1075036748@qq.com>
2017-06-23 19:40:41 +0800

    no message

---
+++ MTSDWebImage/MTSDWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTSDWebImage
+//
+//  Created by user on 2017/6/23.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTSDWebImage/MTSDWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTSDWebImage
+//
+//  Created by user on 2017/6/23.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
